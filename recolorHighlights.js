191be54c1568b14a54afd8107ae3d2253be13545
Michelle Z <mzulli@users.noreply.github.com>
2019-12-18 15:36:47 -0500

    Create recolorHighlights.js

---
+++ recolorHighlights.js
@@ -0,0 +1,31 @@
+/* Change the color of existing highlights */
+
+// Some predefined colors are:
+//   color.yellow
+//   color.green
+//   color.cyan
+//   color.magenta
+
+// Define your own color(s)
+color.ltcyan = new Array("RGB", .75, 1, 1);
+
+// Choose highlights to change: all, pink, yellow, cyan, green -- checkboxes
+
+// Specify the color to be changed
+var oldColor = color.cyan
+
+// Specify the new highlight color
+var colHilite = color.ltcyan;
+
+var oDoc = event.target;
+ 
+ 
+var aAnnts = oDoc.getAnnots({sortBy:"Author"});
+for(var i=0;i<aAnnts.length;i++)
+{
+   if(aAnnts[i].type == "Highlight" && color.equal(aAnnts[i].strokeColor, oldColor))
+   {
+      aAnnts[i].type = "Highlight";
+      aAnnts[i].strokeColor = colHilite;
+   }
+}
