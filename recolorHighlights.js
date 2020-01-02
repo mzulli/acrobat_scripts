3b978e3e4ca9f3461e546224f2fd20ce8df6aa8e
Michelle Z <mzulli@users.noreply.github.com>
2020-01-02 12:49:34 -0500

    Update recolorHighlights.js

--- recolorHighlights.js
+++ recolorHighlights.js
@@ -1,31 +1,32 @@
 /* Change the color of existing highlights */
 
-// Some predefined colors are:
-//   color.yellow
-//   color.green
-//   color.cyan
-//   color.magenta
+//  Some predefined colors are:
+//    color.yellow
+//    color.green
+//    color.cyan
+//    color.magenta
 
-// Define your own color(s)
-color.ltcyan = new Array("RGB", .75, 1, 1);
+//  Define your own color(s)
+//    color.ltcyan = new Array("RGB", .75, 1, 1);
 
-// Choose highlights to change: all, pink, yellow, cyan, green -- checkboxes
+//  Choose highlights to change: all, pink, yellow, cyan, green -- checkboxes
 
-// Specify the color to be changed
-var oldColor = color.cyan
+//  Specify the color to be changed
+//    var oldColor = color.cyan
 
-// Specify the new highlight color
-var colHilite = color.ltcyan;
+//  Specify the new highlight color
+var colHilite = color.yellow;
 
+// PICKUP HERE -- what does this line do?
 var oDoc = event.target;
- 
- 
-var aAnnts = oDoc.getAnnots({sortBy:"Author"});
+
+var aAnnts = oDoc.getAnnots();
+
 for(var i=0;i<aAnnts.length;i++)
 {
-   if(aAnnts[i].type == "Highlight" && color.equal(aAnnts[i].strokeColor, oldColor))
-   {
-      aAnnts[i].type = "Highlight";
-      aAnnts[i].strokeColor = colHilite;
-   }
+  if(aAnnts[i].type == "Highlight") // && color.equal(aAnnts[i].strokeColor, oldColor)
+  {
+    aAnnts[i].type = "Highlight";
+    aAnnts[i].strokeColor = colHilite;
+  }
 }
